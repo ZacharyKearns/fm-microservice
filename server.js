1b2d29421e137b0906b46cd9b892391b6348f6be
Zachary Kearns <zach_kearns89@live.com>
2016-09-10 18:25:41 -0400

    add bing api

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express = require('express');
+var request = require('request');
 var app = express();
-var API_KEY = process.env.GOOGLE_API_KEY;
+var API_KEY = process.env.BING_API_KEY;
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.redirect('https://www.googleapis.com/customsearch/v1?key=' + API_KEY + '&cx=017576662512468239146:omuauf_lfve&q=lectures');
+var options = {
+  url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=sailing+dinghies&mkt=en-us',
+  headers: {
+    'Ocp-Apim-Subscription-Key': API_KEY
+  }
+};
+
+app.get('/', function(req, res) {
+
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      res.send(info);
+    }
+  });
+
 });
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
