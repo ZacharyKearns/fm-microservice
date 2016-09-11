8948b030c626d1ba10ccabbd1b86fe444f5d7be9
Zachary Kearns <zach_kearns89@live.com>
2016-09-10 20:47:29 -0400

    add assert

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 var express = require('express');
+var assert = require('assert')
 var request = require('request');
 var app = express();
 var API_KEY = process.env.BING_API_KEY;
 
 app.set('port', (process.env.PORT || 5000));
 
-var options = {
-  url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=sailing+dinghies&mkt=en-us',
-  headers: {
-    'Ocp-Apim-Subscription-Key': API_KEY
-  }
-};
-
 app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-  request(options, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body);
-      res.send(info);
+app.get('/:query', function(req, res) {
+
+  var options = {
+    url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=' + req.params.query + '&mkt=en-us',
+    headers: {
+      'Ocp-Apim-Subscription-Key': API_KEY
+    }
+  };
+
+  request(options, function(err, response, body) {
+    assert.equal(err, null);
+    if (!err && response.statusCode == 200) {
+      var results = JSON.parse(body);
+      var arr = [];
+
+      for (var i = 0; i < results.value.length; i++) {
+        arr.push({
+          "url": results.value[i].contentUrl,
+          "snippet": results.value[i].name,
+          "thumbmail": results.value[i].thumbnailUrl,
+          "context": results.value[i].hostPageUrl
+        })
+      }
+
+      res.send(arr);
     }
   });
 
